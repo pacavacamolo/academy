4fdfa1f5eab89037606384cbf226a8b7d52c1d0a
Anton_Bandarchyk@epam.com <Anton_Bandarchyk@epam.com>
2016-11-03 17:19:35 +0300

    lesson 5

--- src/theory/lecture_5/object/Main.java
+++ src/theory/lecture_5/object/Main.java
@@ -1,6 +1,10 @@
 package theory.lecture_5.object;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+    private static Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) throws Throwable {
         //все объекты являются наследниками Object, т.е. имеют все неприватные методы.
         Main object1 = new Main();
@@ -17,10 +21,12 @@ public class Main {
         Class clazz = object1.getClass();
 
         //для создания копии объекта
-        object1.clone();
+        //object1.clone();
 
         //выполняется перед удалением объекта сборщиком мусора, на практике не нужно его использовать
         object1.finalize();
+
+        logger.info("1111111111111111111");
 
         //wait notify notifyAll - методы для работы в многопоточном приложении
     }
